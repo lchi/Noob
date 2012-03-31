4dd1287bcd05be0237ea5866b27125034d15bf32
Lucas Chi <chi.lucas@gmail.com>
2012-03-31 13:49:08 -0400

    camera semi-working;

--- main.lua
+++ main.lua
@@ -1,3 +1,5 @@
+require('camera')
+
 grav = 400
 worldWidth = 3000
 worldHeight = 650
@@ -26,6 +28,9 @@ function love.load()
   objects.leftWall.body = love.physics.newBody(world, 25, worldHeight/2, 0, 0) --remember, the body anchors from the center of the shape
   objects.leftWall.shape = love.physics.newRectangleShape(objects.leftWall.body, 0, 0, 50, worldHeight, 0) --anchor the shape to the body, and make it a width of 650 and a height of 50
 
+  objects.leftWall.shape:setFriction(0)
+  objects.rightWall.shape:setFriction(0)
+
   objects.hamster = {}
   objects.hamster.body = love.physics.newBody(world, 650/2, 650/2, 15, 0) --place the body in the center of the world, with a mass of 15
   objects.hamster.shape = love.physics.newCircleShape(objects.hamster.body, 0, 0, 20) --the ball's shape has no offset from it's body and has a radius of 20
@@ -45,9 +50,11 @@ function love.update(dt)
 
   local px, py = objects.hamster.body:getPosition()
   if love.keyboard.isDown("up") and py > 580 then
-    objects.hamster.body:applyImpulse(0,900, 0,0)
+    objects.hamster.body:applyImpulse(0,1200, 0,0)
   end
   world:update(dt)
+  camera:setPosition(px - 300, py - worldHeight+ 50)
+
 --[[
    if x < 0 then
      x = 0
@@ -64,6 +71,7 @@ function love.update(dt)
 end
 
 function love.draw()
+  camera:set()
   --love.graphics.draw(hamster, x, y)
   love.graphics.setColor(72, 160, 14) -- set the drawing color to green for the ground
   love.graphics.polygon("fill", objects.ground.shape:getPoints()) -- draw a "filled in" polygon using the ground's coordinates
@@ -74,4 +82,9 @@ function love.draw()
   love.graphics.setColor(193, 47, 14) --set the drawing color to red for the ball
   love.graphics.circle("fill", objects.hamster.body:getX(), objects.hamster.body:getY(), objects.hamster.shape:getRadius(), 20) -- we want 20 line segments to form the "circle"
 
+  camera:unset()
+end
+
+function math.clamp(x, min, max)
+  return x < min and min or (x > max and max or x)
 end
